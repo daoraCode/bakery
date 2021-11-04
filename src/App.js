93df5fb8fb09feeb39eff9f332f8ed40aa3a1093
daoraCode <severin.mboukou@gmail.com>
2021-11-04 09:58:06 +0100

    Update files

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css'
 import Button from './components/Button'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 class App extends Component {
-  constructor() {
+  constructor () {
     super()
 
     // initial state
     this.state = {
       activateTab: "add",
-      items: []
+      items: [],
     }
 
     // bindings
@@ -18,18 +18,22 @@ class App extends Component {
 
   // methods
   handleButtonClick(tab) {
-    console.log('#1 activateTab :', tab)  
+    console.log('#1 activateTab :', tab)
     this.setState({ activateTab: tab });
   }
-  
+
   render() {
-    console.log("render App activateTab : ",this.state.activateTab);
-    return (
-      <div className="container my-5 me-4 d-flex" role="group">
-        <Button name="Add" handleClick={this.handleButtonClick}/>
-        <Button name="List" handleClick={this.handleButtonClick}/>
-        <Button name="Pay" handleClick={this.handleButtonClick}/>
-      </div>
+    console.log("render App activateTab : ", this.state.activateTab);
+    return ( 
+      <>
+        <h1>Bakery</h1>
+        <div className="container my-5 me-4 d-flex" role="group">
+          <Button isSelected={this.state.activateTab === "Add"} name="Add" handleClick={this.handleButtonClick} />
+          <Button isSelected={this.state.activateTab === "List"} name="List" handleClick={this.handleButtonClick} />
+          <Button isSelected={this.state.activateTab === "Pay"} name="Pay" handleClick={this.handleButtonClick} />
+          {/* {isSelected === "Add" && <Add />} */}
+        </div>
+      </>
     );
   }
 }
--- src/components/Button.js
+++ src/components/Button.js
@@ -2,11 +2,12 @@ import React, { Component } from 'react';
 
 class Button extends Component {
     render() {
-        const { handleClick, name } = this.props;
+        const { handleClick, name, isSelected } = this.props;
+        
         return (
             <button 
                 type="button" 
-                className="btn btn-outline-primary me-5" 
+                className={isSelected ? 'btn btn-primary me-5' : 'btn btn-outline-primary me-5'}
                 onClick={() => handleClick(name)}
             >
                 {name}
